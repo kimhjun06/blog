48b8449412de0af3e1f0ae8e3c665e87ffce63f5
Hyeongjun Kim <kimhjun06@gmail.com>
2025-04-11 15:37:14 +0900

    사이트의 주 색상과 텍스트 색상 변경

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 const siteConfig = {
   username: "", // GitHub 사용자 이름
   repositoryName: "", // GitHub 저장소 이름
-  mainColor: "#3498db", // 사이트의 주 색상
-  textColor: "#333333", // 기본 텍스트 색상
+  mainColor: "#FFFFFF", // 사이트의 주 색상
+  textColor: "#555555", // 기본 텍스트 색상
   blogTitle: "김형준", // 블로그 제목
 };
 
